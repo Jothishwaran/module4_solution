0d7e6ed58d4def608504e2b4551dd716ae5cf4d5
Jothishwaran Natarajan <83685081+Jothishwaran@users.noreply.github.com>
2021-07-07 10:16:41 +0530

    Update SpeakGoodBye.js

--- week4/SpeakGoodBye.js
+++ week4/SpeakGoodBye.js
@@ -1,1 +1,10 @@
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.byeSpeaker = byeSpeaker;
+})(window);
 
